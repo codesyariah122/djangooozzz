190d799853772fda8d9fcb0d0a6f0c4a4dc142f9
codesyariah122 <pujiermanto@gmail.com>
2021-03-26 19:04:31 +0700

    update package

--- django-project/mywebsite/urls.py
+++ django-project/mywebsite/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import url
 from django.contrib import admin
 from . import views
 from blog import views as blogViews
+from about import views as aboutViews
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-
+    url(r'^about/', aboutViews.index),
     url(r'^blog/$', blogViews.index),
 
     url(r'^$', views.index)
